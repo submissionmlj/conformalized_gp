825fe327475be0d8414574406f80d9d1f5bf09ef
Olivier Petit <opetit@quantmetry.com>
2023-10-24 15:36:05 +0000

    UPD: improve plot functions to plot different correlations and MSE of same plot

--- notebooks_gp/plot_functions.py
+++ notebooks_gp/plot_functions.py
@@ -1,5 +1,8 @@
 import numpy as np
+import scipy
 import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
 
 EDF_BLUE = np.array([[26, 54, 105]]) / 255
 EDF_ORANGE = np.array([[223, 84, 49]]) / 255
@@ -77,23 +80,65 @@ def plot_distribution(distribution, model_name, xlabel, ax):
     ax.set_xlim(0, max(1, max(distribution)))
 
 
-def plot_width_error(model, model_name, ci, mean_value, ci_name, ax, r, c, y=None):
+def get_max_width(models, index_confidence):
+    max_width = 0
+    for model in models.values():
+        max_width = max(max_width, model["width"][index_confidence])
+    return max_width
+
+
+def plot_width_error(model, model_name, ax, r, c, max_width=None, y=None, plot_tree=True, plot_rank=True):
     if y is not None:
         error = model["errors"] / y
     else:
         error = model["errors"]
-    ax[r, c].scatter(
-        model["width"],
-        error,
-        s=10,
-        c=model["color"]
-    )
+    if plot_rank:
+        ax[r, c].scatter(
+            scipy.stats.rankdata(model["width"]),
+            scipy.stats.rankdata(error),
+            s=10,
+            c=model["color"]
+        )
+    else:
+        ax[r, c].scatter(
+            model["width"],
+            error,
+            s=10,
+            c=model["color"]
+        )
+    if max_width:
+        ax[r, c].set_xlim(0, max_width * 1.1)
+    ci_pearson_correlation = model["pearson_correlation_to_error"].confidence_interval
+    mean_pearson_correlation = np.mean(model["pearson_correlation_to_error"].bootstrap_distribution)
+    ci_spearman_correlation = model["spearman_correlation_to_error"].confidence_interval
+    mean_spearman_correlation = np.mean(model["spearman_correlation_to_error"].bootstrap_distribution)
+    ci_mse = model["tree_mse"].confidence_interval
+    mean_mse = model["tree_mse"].bootstrap_distribution.mean()
     ax[r, c].set_xlabel("Width of the prediction interval\n", size=15)
     ax[r, 0].set_ylabel("Relative error of the metamodel", size=15)
     ax[r, c].set_title(
         model_name + "\n" +
-        r"$\bf{" + ci_name + r"\in [}$" + r"$\bf{" + str(round(ci[0], 2)) +
-        ", " + str(round(ci[1], 2)) + "], mean = " + str(round(mean_value, 2)) + "}$" + "\n" +
+        r"$\bf{Pearson correlation\in [}$" + r"$\bf{" + str(round(ci_pearson_correlation[0], 2)) +
+        ", " + str(round(ci_pearson_correlation[1], 2)) + "], mean = " +
+        str(round(mean_pearson_correlation, 2)) + "}$" + "\n" +
+
+        r"$\bf{Spearman correlation\in [}$" + r"$\bf{" + str(round(ci_spearman_correlation[0], 2)) +
+        ", " + str(round(ci_spearman_correlation[1], 2)) + "], mean = "
+        + str(round(mean_spearman_correlation, 2)) + "}$" + "\n" +
+
+        r"$\bf{MSE\in [}$" + r"$\bf{" + str(round(ci_mse[0], 2)) +
+        ", " + str(round(ci_mse[1], 2)) + "], mean = " + str(round(mean_mse, 2)) + "}$" + "\n" +
+
         r"$\bf{Global MSE =" + str(round(np.mean(model["errors"] ** 2), 2)) + "}$"
     )
-    # ax[r, c].set_xlim(0, 7)
+    if plot_tree:
+        _plot_tree(model, r, c, ax)
+
+
+def _plot_tree(model, r, c, ax):
+    X_temp = model["width"]
+    y_temp = model["errors"]
+    X_temp_train, X_temp_test, y_temp_train, y_temp_test = train_test_split(X_temp, y_temp, test_size=.3)
+    tree = DecisionTreeRegressor(min_samples_split=3, max_depth=3).fit(X_temp_train.reshape(-1, 1), y_temp_train)
+    pred = tree.predict(np.arange(0, X_temp.max(), .1).reshape(-1, 1))
+    ax[r, c].plot(np.arange(0, X_temp.max(), .1), pred)
