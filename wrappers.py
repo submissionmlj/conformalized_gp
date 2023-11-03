488d89deb2cec9293c5515ba1cb016a710dc6c34
Olivier Petit <opetit@quantmetry.com>
2023-11-03 17:36:24 +0000

    ADD: add bagging gp model  and cpu_bagging

--- wrappers.py
+++ wrappers.py
@@ -97,3 +97,66 @@ class GpOTtoSklearnStd(BaseEstimator):
             return True
         else:
             return False
+
+
+class BaggedGP(BaseEstimator):
+    """
+    Standard-deviation conformal score for GP
+    """
+    def __init__(
+            self, n_estimators, scale: int, amplitude: float,
+            nu: float, noise: float = None, power_std: float = 1
+    ) -> None:
+        self.n_estimators = n_estimators
+        self.scale = scale
+        self.amplitude = amplitude
+        self.nu = nu
+        self.trained_ = False
+        self.noise = noise
+        self.power_std = power_std
+
+    def _initiate_estimators(self):
+        estimators = []
+        for _ in range(self.n_estimators):
+            estimators.append(
+                GpOTtoSklearnStd(
+                    scale=self.scale, amplitude=self.amplitude,
+                    nu=self.nu, noise=self.noise, power_std=self.power_std
+                )
+            )
+        return estimators
+
+    def _initiate_databags(self, X_train, y_train):
+        databags = []
+        n_points = len(X_train)
+        for i in range(self.n_estimators):
+            indices = np.random.choice(n_points, n_points // 2, replace=False)
+            databags.append((X_train[indices], y_train[indices]))
+
+        return databags
+
+    def fit(self, X_train, y_train):
+        self.estimators = self._initiate_estimators()
+        databags = self._initiate_databags(X_train, y_train)
+
+        for i in range(self.n_estimators):
+            X, y = databags[i]
+            self.estimators[i].fit(X, y)
+
+    def predict(self, X_test, return_std=False):
+        preds, std = [], []
+        for i in range(self.n_estimators):
+            preds_temp, std_temp = self.estimators[i].predict(X_test, return_std=True)
+            preds.append(preds_temp)
+            std.append(std_temp)
+
+        if not return_std:
+            return np.array(preds).mean(axis=0)
+        else:
+            return np.array(preds).mean(axis=0), np.array(std).mean(axis=0)
+
+    def __sklearn_is_fitted__(self):
+        if self.trained_:
+            return True
+        else:
+            return False
